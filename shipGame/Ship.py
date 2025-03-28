bb922d3b0b3ca0b781b579d6f23ee3d1f3eb5db8
Luke <lukebeekham@gmail.com>
2025-03-28 13:18:38 -0700

    Added Lazers that the player can shoot

--- shipGame/Ship.py
+++ shipGame/Ship.py
@@ -22,9 +22,14 @@ class BackEnd:
     def LoadImages(self):
         Ship = pygame.image.load(self.image_Folder + "\Ship.png").convert_alpha()
         MachineSoulEnemy1 = pygame.image.load(self.image_Folder + "\MachineSoulEnemy1.png").convert_alpha()
+        ShipLazer = pygame.image.load(self.image_Folder + "\ShipLazer.png").convert_alpha()
+        EnemyLazer = pygame.image.load(self.image_Folder + "\EnemyLazer.png").convert_alpha()
         self.imageList = {
             "Ship" : Ship,
-            "MachineSoulEnemy1" : MachineSoulEnemy1
+            "MachineSoulEnemy1" : MachineSoulEnemy1,
+            "ShipLazer" : ShipLazer,
+            "EnemyLazer" : EnemyLazer
+            
         }
         return self.imageList
 
@@ -56,7 +61,40 @@ class Entity():
         angle = math.degrees(angle)
         return angle
     
+    def ShootLazers(self,x,y,angle,vel,img):
+        rect = img.get_rect()
 
+        
+
+        rect.x = x
+        rect.y = y
+
+
+
+        img_x = rect.x
+        img_y = rect.y
+        img_x += vel * math.cos(math.radians(angle))
+        img_y += vel * math.sin(math.radians(angle))
+
+        img_x = rect.x
+        img_y = rect.y
+
+        self.Draw(img,img_x,img_y)
+        return rect
+    
+    def UpdateLazers(self,vel,img,angle,rect):
+        
+        rect.x += vel * math.cos(math.radians(angle))
+        rect.y += vel * math.sin(math.radians(angle))
+
+        if rect.x > Game.width or rect.x < 0 or rect.y > Game.height or rect.y < 0:
+            return False
+        else:
+            self.Draw(img,rect.x,rect.y)
+            return True
+
+    
+    
     
 
     
@@ -70,7 +108,7 @@ class Enemy:
         self.y = y
         self.width = width
         self.height = height
-        self.vel = 0.1
+        self.vel = 2
         self.angle = 0
         self.type = type
         self.orginalImage = Game.imageList[type]
@@ -96,28 +134,121 @@ class Ship:
     shipImage = originalShipImage.copy()
     shipImage = pygame.transform.scale(shipImage, (128, 128))
     originalScaledShipImage = shipImage.copy()
+
+    shipLazer = Game.imageList["ShipLazer"]
+    shipLazer = pygame.transform.scale(shipLazer, (10, 10))
+    
     
     def __init__(self):
-        self.x = 50
-        self.y = 50
+        self.x = Game.width / 2
+        self.y = Game.height / 2
         self.width = 50
         self.height = 50
-        self.vel = 0
+        self.vel = 5
         self.color = (0, 255, 0)
         self.angle = 0
+        self.LazerList = []
+        self.LazerHeatBackground = Drawing(25,25,120,50,(0,0,0),(0,0,0),"rect")
+        self.LazerHeatBar = Drawing(37.5,37.5,100,25,(0,255,0),(0,255,0),"rect")
+        self.LazersFired = 0
+        self.LazerCooldown = False
 
-    def Draw(self, win):
+
+    def Draw(self):
         Entity.Draw(Ship.shipImage, self.x, self.y)
 
     def Rotate(self, angle):
         Ship.shipImage = Entity.Rotate(Ship.originalScaledShipImage, angle)
 
-    def Move(self,vel): 
+    def Move(self,direction):
+        vel = direction * self.vel 
         self.x,self.y = Entity.Move(self.x,self.y,self.angle,vel)
+    
+    def ShootLazer(self):
+        if self.LazersFired >= 100:
+            self.LazerCooldown = True
+        
+        if self.LazerCooldown:
+            self.LazerHeatBar.color = (255,0,0)
+            self.LazerHeatBar.fillColor = (255,0,0)
 
+            return
+        
+        NewLazer = self.shipLazer.copy()
+
+        rect = Entity.ShootLazers(self.x,self.y,self.angle,10,NewLazer)
+        self.LazerList.append((NewLazer,self.angle,rect))
+
+        self.LazersFired += 1
+        self.LazerHeatBar.width = 100 - (self.LazersFired) 
+        
+        self.LazerHeatBar.draw()
+    
+    def Update(self):
+        self.Draw()
+        for tuple in self.LazerList:
+            lazer = tuple[0]
+            angle = tuple[1]
+            rect = tuple[2]
+
+
+            # Access the coordinates
+            x = rect.x
+            y = rect.y
+
+            if not(Entity.UpdateLazers(20,lazer,angle,rect)):
+                self.LazerList.remove(tuple)
+        if self.LazersFired > 0:
+            self.LazersFired -= 0.1
+            self.LazerHeatBar.width = 100 - (self.LazersFired) 
+            self.LazerHeatBar.draw()
+        
+        if self.LazersFired <= 0 and self.LazerCooldown:
+                self.LazerCooldown = False
+                self.LazersFired = 0
+                self.LazerHeatBar.color = (0,255,0)
+                self.LazerHeatBar.fillColor = (0,255,0)
+                self.LazerHeatBar.draw()
+
+
+class Drawing:
+    drawingList = []
+    def __init__(self,x,y,width,height,color,fillcolor,type):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        if fillcolor == None:
+            self.fillColor = None
+        self.fillColor = fillcolor
+        self.type = type
+
+
+
+        Drawing.drawingList.append(self)
+
+    def MakeSurface(self):
+        if self.type == "rect":
+            rect_surface = pygame.Surface((self.width, self.height)) 
+            if self.fillColor != None:
+                rect_surface.fill(self.fillColor) 
+        self.surface = rect_surface
+
+    def draw(self):
+        if self.type == "rect":
+            self.MakeSurface()
+            Game.win.blit(self.surface, (self.x, self.y))
+
+    @classmethod
+    def UpdateDrawings(cls):
+        for drawing in cls.drawingList:
+            drawing.draw()    
+        
 
 plr = Ship()
 Enemy1 = Enemy(100,100,50,50,"MachineSoulEnemy1")
+clock = pygame.time.Clock()
 while Game.run:
     Game.win.fill((255, 255, 255))
     for event in pygame.event.get():
@@ -127,21 +258,24 @@ while Game.run:
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_a]:
-        plr.angle -= 0.5
+        plr.angle -= 5
         plr.Rotate(plr.angle)
     if keys[pygame.K_d]:
-        plr.angle += 0.5
+        plr.angle += 5
         plr.Rotate(plr.angle)
     if keys[pygame.K_w]:
-        plr.Move(0.75)
+        plr.Move(1)
     if keys[pygame.K_s]:
-        plr.Move(-0.75)
+        plr.Move(-1)
+    if keys[pygame.K_SPACE]:
+        plr.ShootLazer()
 
     Enemy1.Update()
     Enemy1.Draw()
-    plr.Draw(Game.win)
+    plr.Update()
+    Drawing.UpdateDrawings()
     pygame.display.update()
         
-    
+    clock.tick(60)
 
 pygame.quit()
