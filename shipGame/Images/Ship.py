565ae9728cb6239fab96fb108bfcaf36b722e82e
Luke <lukebeekham@gmail.com>
2025-04-24 11:13:31 -0700

    Added Wave Based enemies, And a Main menu

--- shipGame/Images/Ship.py
+++ shipGame/Images/Ship.py
@@ -13,9 +13,6 @@ pygame.mixer.init()
 
 pygame.mixer.set_num_channels(8) 
 
-
-
-
 # Get the directory containing the current file
 
 class BackEnd:
@@ -29,6 +26,29 @@ class BackEnd:
         self.sound_Folder = self.current_dir + "\Sounds"
         self.music_Folder = self.current_dir + "\Music"
         self.imageList = []
+        self.wave = 0
+        self.WaveImageTimer = self.CreateTimer(5000)
+        self.StopTimer(self.WaveImageTimer)
+        self.WaveImage = None
+        self.isWaveImage = False
+        self.WaveTimer = self.CreateTimer(20000)
+        self.StopTimer(self.WaveTimer)
+        self.wave1List = ["1","1","1","1","1","1","1","1","1","1"]
+        self.wave2List = ["2","1","2","1","2","2","2","2","1","1","2","1","2","1","2","1","2","1","2","1"]
+        self.wave3List = ["3","2","1","2","3","2","1","1","1","1","2","1","2","1","2","1","2","1","2","1","1","1","2","1","2","1","2","1","2","1","3","3"]
+        self.wave4List = ["4","3","2","1","2","3","4","4","2", "1","4","1","4","3","2","1","2","3","4","4" ,"2", "1","4","1","4","4","2","1","2","3","4","4" ,"2", "1","4","1","4" ,"2", "1","4","1"]
+        self.wave5List = ["4","3","2","1","2","3","4","4","2", "1","4","1","4","3","2","1","2","3","4","4" ,"2", "1","4","1","4","4","2","1","2","3","4","4" ,"2", "1","4","1","4" ,"2", "1","4","1","3","3","2","1","4","2","1","2","4","4"]
+
+        self.wave1List = random.sample(self.wave1List, len(self.wave1List))
+        self.wave2List = random.sample(self.wave2List, len(self.wave2List))
+        self.wave3List = random.sample(self.wave3List, len(self.wave3List))
+        self.wave4List = random.sample(self.wave4List, len(self.wave4List))
+        self.wave5List = random.sample(self.wave5List, len(self.wave5List))
+
+        self.isMainMenu = True
+
+
+
     def LoadImages(self):
         Ship = pygame.image.load(self.image_Folder + "\Ship.png").convert_alpha()
         MachineSoulEnemy1 = pygame.image.load(self.image_Folder + "\MachineSoulEnemy1.png").convert_alpha()
@@ -39,6 +59,17 @@ class BackEnd:
         EnemyLazer = pygame.image.load(self.image_Folder + "\EnemyLazer.png").convert_alpha()
         TargetCursor = pygame.image.load(self.image_Folder + "\TargetCursor.png").convert_alpha()
         Map = pygame.image.load(self.image_Folder + "\Map.png").convert()
+        Wave1 = pygame.image.load(self.image_Folder + "\WAVEONE.png").convert_alpha()
+        Wave2 = pygame.image.load(self.image_Folder + "\WAVETWO.png").convert_alpha()
+        Wave3 = pygame.image.load(self.image_Folder + "\WAVETHREE.png").convert_alpha()
+        Wave4 = pygame.image.load(self.image_Folder + "\WAVEFOUR.png").convert_alpha()
+        Wave5 = pygame.image.load(self.image_Folder + "\WAVEFIVE.png").convert_alpha()
+        MissionComplete = pygame.image.load(self.image_Folder + "\MissionComplete.png").convert_alpha()
+        MissionFailed = pygame.image.load(self.image_Folder + "\MissionFailed.png").convert_alpha()
+        ShipDied = pygame.image.load(self.image_Folder + "\ShipDied.png").convert_alpha()
+        Respawn = pygame.image.load(self.image_Folder + "\Respawn.png").convert_alpha()
+        MainMenu = pygame.image.load(self.image_Folder + "\MainMenu.png").convert_alpha()
+
         self.imageList = {
             "Ship" : Ship,
             "MachineSoulEnemy1" : MachineSoulEnemy1,
@@ -49,6 +80,17 @@ class BackEnd:
             "EnemyLazer" : EnemyLazer,
             "TargetCursor" : TargetCursor,
             "Map" : Map,
+            "Wave1" : Wave1,
+            "Wave2" : Wave2,
+            "Wave3" : Wave3,
+            "Wave4" : Wave4,
+            "Wave5" : Wave5,
+            "MissionComplete" : MissionComplete,
+            "MissionFailed" : MissionFailed,
+            "ShipDied" : ShipDied,
+            "Respawn" : Respawn,
+            "MainMenu" : MainMenu,
+
         }
         return self.imageList
     
@@ -66,7 +108,51 @@ class BackEnd:
     @staticmethod
     def ReStartTimer(Timer,millSeconds):
         pygame.time.set_timer(Timer, millSeconds)
+    
+    
+    def NewWave(self):
+        if self.wave+1 > 5:
+            self.EndGame()
+            return
+        self.isWaveImage = True
+        string = "Wave" + str(self.wave+1)  # Use self.wave directly
+        self.WaveImage = self.imageList[string].copy()
+        
+        self.win.blit(self.WaveImage, (Game.width/2 - int(self.WaveImage.get_width() / 2), 50 - int(self.WaveImage.get_height() / 2)))
+        self.ReStartTimer(self.WaveImageTimer, 5000)
+        
+    
+    def UpdateWaveImage(self):
+        if self.isWaveImage:
+            self.win.blit(self.WaveImage, (Game.width/2 - int(self.WaveImage.get_width() / 2), 50 - int(self.WaveImage.get_height() / 2)))
+            if plr.health <= 0:
+                RespawnImage = self.imageList["Respawn"].copy()
+                self.win.blit(RespawnImage, (Game.width/2 - int(RespawnImage.get_width() / 2), 100 - int(RespawnImage.get_height() / 2)))
 
+    def EndGame(self):
+        self.isWaveImage = True
+        self.WaveImage = self.imageList["MissionComplete"].copy()
+        self.win.blit(self.WaveImage, (Game.width/2 - int(self.WaveImage.get_width() / 2), 50 - int(self.WaveImage.get_height() / 2)))
+
+    def MainMenu(self):
+        MainMenuImage = self.imageList["MainMenu"].copy()
+        self.win.blit(MainMenuImage, (Game.width/2 - int(MainMenuImage.get_width() / 2), 50 - int(MainMenuImage.get_height() / 2)))
+
+    def UpdateMap(self):
+        if self.isMainMenu:
+            self.win.blit(Game.imageList["MainMenu"], (0,0))
+        else:
+            self.win.blit(Game.imageList["Map"], (0, 0))
+    
+    def ClickedButton(self,x,y):
+        if not(self.isMainMenu):
+            return
+        if x >= 992 and x <= Game.width and y >= 304 and y <= 374:
+            self.isMainMenu = False
+            self.NewWave()
+        elif x >= 998 and x <= Game.width and y >= 460 and y <= 526:
+            pass # Add tutoral here!
+    
 Game = BackEnd()
 
 Game.LoadImages()
@@ -93,7 +179,6 @@ class Music:
         if self.played:
             pygame.mixer.music.stop() 
             self.played = False
-
 class Sound:
     Channel = -1
     def __init__(self, sound,vol):
@@ -145,9 +230,6 @@ class Entity:
         angle = math.atan2(targetY - y, targetX - x)
         angle = math.degrees(angle)
         return angle
-    
-
-
     
     
 Entity = Entity()
@@ -212,8 +294,8 @@ class Lazer:
         if self.type == "Ship":
             for enemy in Enemy.EnemyList:
                 # Correct the offset calculation for mask overlap
-                offset_x = self.rect.x - int(enemy.x - enemy.image.get_width() / 2)
-                offset_y = self.rect.y - int(enemy.y - enemy.image.get_height() / 2)
+                offset_x = self.rect.x - int(enemy.rect.x - enemy.image.get_width() / 2)
+                offset_y = self.rect.y - int(enemy.rect.y - enemy.image.get_height() / 2)
                 if enemy.mask.overlap(self.mask, (offset_x, offset_y)):
                     enemy.ChangeHealth(-1)
                     self.Remove()
@@ -235,7 +317,7 @@ class Enemy:
 
     Lazer = Game.imageList["EnemyLazer"].copy()
     Lazer = pygame.transform.scale(Lazer, (10, 10))
-    
+    makeEnemies = False
     
     def __init__(self,x,y,type):
         self.x = x
@@ -247,6 +329,8 @@ class Enemy:
         self.image = Game.imageList[type]
         self.mask = pygame.mask.from_surface(self.image)
         self.LazerList = []
+        self.rect = self.image.get_rect()
+        
 
         match type:
             case "MachineSoulEnemy1":
@@ -269,6 +353,8 @@ class Enemy:
 
     @staticmethod
     def createEnemy(type):
+        if not(Enemy.makeEnemies):
+            return
         TopOrSide = random.randint(0,1)
         if TopOrSide == 0:
             x = random.randint(0,Game.width)
@@ -276,6 +362,11 @@ class Enemy:
         else:
             x = 0
             y = random.randint(0,Game.height)
+        if type == "MachineSoulEnemy3":
+            for enemy in Enemy.EnemyList:
+                if enemy.type == "MachineSoulEnemy3":
+                    type == "MachineSoulEnemy1"
+                    break
         Enemy1 = Enemy(x,y,type)
         
     def Draw(self):
@@ -283,6 +374,9 @@ class Enemy:
     
     def Move(self):
         self.x,self.y = Entity.Move(self.x,self.y,self.angle,self.vel)
+        self.rect.x = self.x 
+        self.rect.y = self.y
+        
     
     def Rotate(self):
         self.image = Entity.Rotate(self.orginalImage,self.angle)
@@ -317,6 +411,9 @@ class Enemy:
             self.health = self.maxHealth
 
     def Update(self):
+        if plr.health <= 0:
+            self.Draw()
+            return
         if self.type == "MachineSoulEnemy1":
             self.angle = Entity.FollowAngle(self.x,self.y,plr.x,plr.y)
             self.Rotate()
@@ -386,6 +483,8 @@ class Enemy:
                 self.HealthBar.fillColor = (255,0,0)
 
             self.HealthBar.draw()
+        self.rect.x = self.x 
+        self.rect.y = self.y
   
         self.Draw()
             
@@ -393,7 +492,7 @@ class Enemy:
     def UpdateEnemies(cls):
         for enemy in cls.EnemyList:
             enemy.Update()
-                
+
 
 
 class Ship:
@@ -404,6 +503,9 @@ class Ship:
 
     shipLazer = Game.imageList["ShipLazer"]
     shipLazer = pygame.transform.scale(shipLazer, (10, 10))
+
+    shipDiedImage = Game.imageList["ShipDied"].copy()
+    shipDiedImage = pygame.transform.scale(shipDiedImage,(32,32))
 
     shipLazerSound = Sound(Game.sound_Folder + "/ShipLazerSound.wav",0.5)
 
@@ -424,21 +526,27 @@ class Ship:
         self.LazersFired = 0
         self.LazerCooldown = False
         self.health = 100
+        self.direction = 0
         
         self.healthBarBackground = Drawing.MakeRect(25,100,50,120,(0,0,0),(130, 127, 128),"rect")
         self.healthBar = Drawing.MakeRect(37.5,112.5,25,100,(130,127,255),(255,0,0),"rect")
         self.mask = pygame.mask.from_surface(self.shipImage)
         self.rect = self.shipImage.get_rect()
         self.rect.center = (self.x, self.y)  
+        self.maxVel = 7
     def Draw(self):
+        if Game.isMainMenu:
+            return
         Entity.Draw(Ship.shipImage, self.x, self.y)
         self.rect.center = (self.x, self.y)
 
     def Rotate(self, angle):
+        if self.health <= 0:
+            return
         Ship.shipImage = Entity.Rotate(Ship.originalScaledShipImage, angle)
 
-    def Move(self,direction):
-        vel = direction * self.vel 
+    def Move(self):
+        vel = self.direction * self.vel 
         self.x,self.y = Entity.Move(self.x,self.y,self.angle,vel)
         if self.x < 0:
             self.x = 0
@@ -449,7 +557,34 @@ class Ship:
         if self.y > Game.height:
             self.y = Game.height
     
+    def IncreaseVelocity(self,vel,direction):
+        if self.health <= 0:
+            return
+        if self.vel <= 0:
+            self.vel = 0
+            self.direction = direction
+        
+
+        if self.direction == direction:    
+            self.vel += vel
+        else:
+            self.vel -= vel
+        if self.vel > self.maxVel:
+            self.vel = self.maxVel
+         
+    
+    def DecreaseVelocity(self,vel):
+        self.vel -= vel
+        if self.vel < 0:
+            self.vel = 0
+        if self.vel > self.maxVel:
+            self.vel = self.maxVel
+    
     def ShootLazer(self):
+        if Game.isMainMenu:
+            return
+        if self.health <= 0:
+            return
         if self.LazersFired >= 100:
             self.LazerCooldown = True
         
@@ -476,10 +611,28 @@ class Ship:
             print("You Died")
         if self.health > 100:
             self.health = 100
+        elif self.health <= 0:
+            self.health = 0
+            Game.StopTimer(Enemy.EnemyTimer)
+            Game.StopTimer(Ship.shipLazerSoundTimer)
+            Game.isWaveImage = True
+            Game.WaveImage = Game.imageList["MissionFailed"].copy()
+            Game.win.blit(Game.WaveImage, (Game.width/2 - int(Game.WaveImage.get_width() / 2), 50 - int(Game.WaveImage.get_height() / 2)))
+            
+            Ship.shipImage = Entity.Rotate(Ship.shipDiedImage, self.angle)
+            return
         self.healthBar.height = self.health 
         self.healthBar.draw()
         self.healthBarBackground.draw()
+    
+
     def Update(self):
+        if self.health <= 0:
+            self.Draw()
+            return
+        if self.vel > 0:
+            self.Move()
+        self.DecreaseVelocity(0.1)
         self.Draw()
         if self.LazersFired > 0:
             self.LazersFired -= 0.1
@@ -492,7 +645,6 @@ class Ship:
                 self.LazerHeatBar.color = (0,255,0)
                 self.LazerHeatBar.fillColor = (0,255,0)
                 self.LazerHeatBar.draw()
-
 
 
 
@@ -554,10 +706,6 @@ class Drawing:
     def UpdateDrawings(cls):
         for drawing in cls.drawingList:
             drawing.draw()    
-        
-
-
-
 
 class TargetCursor:
     def __init__(self,x,y):
@@ -575,22 +723,38 @@ class TargetCursor:
         self.Draw()
 
 plr = Ship()
-Enemy1 = Enemy(100,100,"MachineSoulEnemy1")
 clock = pygame.time.Clock()
 
 Target = TargetCursor(0,0)
 pygame.mouse.set_visible(False)
 
+
+
+
 while Game.run:
-    Game.win.blit(Game.imageList["Map"], (0, 0))
+    Game.UpdateMap()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             Game.run = False
-        if event.type == Enemy.EnemyTimer:
-            randomNum = random.randint(0,3)
-            randomNum += 1
-            enemyType = "MachineSoulEnemy" + str(randomNum)
-            Enemy.createEnemy(enemyType)
+        if event.type == Enemy.EnemyTimer and not Game.isWaveImage and not Game.wave > 5 and not Game.wave == 0 and not Game.isMainMenu:
+            waveList = []
+            match Game.wave:
+                case 1:
+                    waveList = Game.wave1List
+                case 2:
+                    waveList = Game.wave2List
+                case 3:
+                    waveList = Game.wave3List
+                case 4:
+                    waveList = Game.wave4List
+                case 5:
+                    waveList = Game.wave5List
+                case _:
+                    print("NO WAVE")
+            if not(len(waveList) == 0):
+                num = waveList.pop(0)
+                enemyType = "MachineSoulEnemy" + str(num)
+                Enemy.createEnemy(enemyType)
         if event.type == Ship.shipLazerSoundTimer:
             Ship.shipLazerSound.play()
             Game.StopTimer(Ship.shipLazerSoundTimer)
@@ -598,22 +762,68 @@ while Game.run:
             for enemy in Enemy.EnemyList:
                 if enemy.type == "MachineSoulEnemy2":
                     enemy.ShootLazers()
-
+        if event.type == Game.WaveImageTimer:
+            Game.wave += 1
+            Game.StopTimer(Game.WaveImageTimer)
+            waveList = []
+            match Game.wave:
+                case 1:
+                    waveList = Game.wave1List
+                case 2:
+                    waveList = Game.wave2List
+                case 3:
+                    waveList = Game.wave3List
+                case 4:
+                    waveList = Game.wave4List
+                case 5:
+                    waveList = Game.wave5List
+            if waveList:
+                Game.ReStartTimer(Enemy.EnemyTimer, 20000 // len(waveList))  # Adjust EnemyTimer dynamically
+                Game.isWaveImage = False
+                Game.ReStartTimer(Game.WaveTimer, 20000)
+                Enemy.makeEnemies = True
+        if event.type == Game.WaveTimer:
+            Enemy.makeEnemies = False
+            if len(Enemy.EnemyList) > 0:
+                Game.ReStartTimer(Enemy.EnemyTimer,1000)
+            else:
+                Game.NewWave()
+                if not(Game.wave >= 5):
+                    Game.NewWave()
+                    Game.StopTimer(Game.WaveTimer)
+                    Game.ReStartTimer(Enemy.EnemyTimer,5000 - Game.wave * 500)
+                
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_a]:
-        plr.angle -= 5
-        plr.Rotate(plr.angle)
-    if keys[pygame.K_d]:
-        plr.angle += 5
-        plr.Rotate(plr.angle)
-    if keys[pygame.K_w]:
-        plr.Move(1)
-    if keys[pygame.K_s]:
-        plr.Move(-1)
-    
+    if not(Game.isMainMenu): 
+        if keys[pygame.K_a]:
+            plr.angle -= 5
+            plr.Rotate(plr.angle)
+        if keys[pygame.K_d]:
+            plr.angle += 5
+            plr.Rotate(plr.angle)
+        if keys[pygame.K_w]:
+            plr.IncreaseVelocity(1,1)
+            
+        if keys[pygame.K_s]:
+            plr.IncreaseVelocity(1,-1)
+        
+        if keys[pygame.K_r]:
+            if plr.health <= 0:
+                Enemy.EnemyList.clear()
+                print(Enemy.EnemyList)
+                plr.ChangeHealth(100) 
+                plr.shipImage = Ship.originalScaledShipImage.copy()
+                Ship.shipImage = plr.shipImage
+                plr.Rotate(plr.angle)
+                Game.isWaveImage = False
+                Game.wave = 0
+
+
     mouse_buttons = pygame.mouse.get_pressed()
     if mouse_buttons[0]:
+        Game.ClickedButton(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1])
+        
         plr.ShootLazer()
 
     Enemy.UpdateEnemies()
@@ -621,8 +831,10 @@ while Game.run:
     Drawing.UpdateDrawings()
     Target.Update()
     Lazer.UpdateLazers()
+    Game.UpdateWaveImage()
     pygame.display.update()
         
     clock.tick(60)
+    
 
 pygame.quit()
